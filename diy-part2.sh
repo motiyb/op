1162cb777a2b238982f27333f4aa3c09e78f49f0
motiyb <87536739+motiyb@users.noreply.github.com>
2022-06-04 21:49:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,9 +19,6 @@ cp /usr/bin/upx $GITHUB_WORKSPACE/openwrt/staging_dir/host/bin/
 cp /usr/bin/upx-ucl $GITHUB_WORKSPACE/openwrt/staging_dir/host/bin/
 
 #Prepare openclash
-curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-286.tar.gz -o /tmp/clash.tar.gz
-tar zxvf /tmp/clash.tar.gz -C /tmp
-chmod +x /tmp/clash
-mkdir -p $GITHUB_WORKSPACE/openwrt/package/luci-app-openclash/files/etc/openclash/core
-mv /tmp/clash $GITHUB_WORKSPACE/openwrt/package/luci-app-openclash/files/etc/openclash/core/clash
-rm -rf /tmp/clash.tar.gz
+wget https://github.com/vernesong/OpenClash/archive/master.zip
+unzip master.zip
+cp -r OpenClash-master/luci-app-openclash $GITHUB_WORKSPACE/openwrt/package
