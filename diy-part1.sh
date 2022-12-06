92072dd2b136a3e767bf4b9696ef5e5d7fe29dbc
motiyb <87536739+motiyb@users.noreply.github.com>
2022-12-06 22:55:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,8 +10,6 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 #Prepare openclash
 wget https://github.com/vernesong/OpenClash/archive/master.zip
@@ -20,7 +18,8 @@ cp -r OpenClash-master/luci-app-openclash $GITHUB_WORKSPACE/openwrt/package
 
 # Add a feed source
 
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+sed -i "/helloworld/d" "feeds.conf.default"
+echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 cat >> feeds.conf.default <<EOF
